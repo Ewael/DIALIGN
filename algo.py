8a16aff565b3807d28d49fdb5352eaef73106c83
Ewael <turodoras@gmail.com>
2022-01-07 09:38:58 -0500

    Add base struct for algo

---
+++ algo.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+
+'''
+This file is an implementation of the DIALIGN algorithm for sequences alignement
+detection. It uses FASTA format as input and DIALIGN format as output.
+
+References:
+
+> DIALIGN manual (EN)
+http://dialign.gobics.de/anchor/manual
+
+> Input and output details (EN)
+https://www.genomatix.de/online_help/help_dialign/dialign2_help.html
+
+> `lingpy` documention (EN)
+http://lingpy.org/reference/lingpy.align.html
+
+> Algorithm details (FR)
+https://wikis.univ-lille.fr/bilille/_media/ib2019-2-partie2-alignement.pdf
+'''
+
+
+def main():
+
+
+
+if __name__ == '__main__':
+    main()
